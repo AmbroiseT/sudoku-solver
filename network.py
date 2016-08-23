e6b3815be9e1c03de83bdf419099a117f3bfc8cc
AmbroiseT <ambroise.thomine@free.fr>
2016-08-23 15:39:27 +0200

    Improvement and bug correction

--- network.py
+++ network.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 import scipy
 import scipy.optimize
+import scipy.io as sio
 
 train_size = 9
 image_size = 28
@@ -14,21 +15,26 @@ def sigmoid(mat):
 def sigmoidGradient(z):
     return sigmoid(z)*(1-sigmoid(z));
 
-def predict(Theta, input_layer_size, hidden_layer_size, num_labels, X):
-    theta0 = np.array(Theta[0:(input_layer_size+1)*hidden_layer_size])
-    theta1 = np.array(Theta[(input_layer_size+1)*hidden_layer_size:])
-    theta0 = theta0.reshape((input_layer_size+1, hidden_layer_size))
-    theta1 = theta1.reshape((hidden_layer_size+1, num_labels))
+def predict(nn_params, input_layer_size, hidden_layer_size, num_labels, X):
+    theta1 = np.array(nn_params[0:(input_layer_size+1)*hidden_layer_size])
+
+    theta2 = np.array(nn_params[(input_layer_size+1)*hidden_layer_size:])
+    theta1 = theta1.reshape((hidden_layer_size, input_layer_size+1), order='F')
+    theta2 = theta2.reshape((num_labels, hidden_layer_size+1), order='F')
+
     m = X.shape[0]
     X = np.hstack((np.ones([m,1]),X))
+    a1 = X
+    z2 = np.dot(a1, theta1.T)
+    a2 = sigmoid(z2)
     
-    a1 = sigmoid(X.dot(theta0))
     
     #a1 = np.vstack((np.ones([1,a1.shape[1]]),a1)) 
-    a1 = np.append(np.ones([a1.shape[0], 1]),a1, 1) #Add bias
+    a2 = np.append(np.ones([a1.shape[0], 1]),a2, 1) #Add bias
 
-    a2 = sigmoid(a1.dot(theta1))
-    return a2
+    z3 = a2.dot(theta2.T)
+    h = sigmoid(z3)
+    return h
 
 def load_x_for_val(val, m):
     img = cv2.imread("data/mnist_train"+str(val)+".jpg", cv2.IMREAD_GRAYSCALE)
@@ -47,77 +53,79 @@ def load_x_for_val(val, m):
 def nn_costfunction(nn_params, *args):
     input_layer_size, hidden_layer_size, num_labels, X, y, lam = args[0], args[1], args[2], args[3], args[4], args[5]
     
-    theta0 = np.array(nn_params[0:(input_layer_size+1)*hidden_layer_size])
-    theta1 = np.array(nn_params[(input_layer_size+1)*hidden_layer_size:])
-    theta0 = theta0.reshape((input_layer_size+1, hidden_layer_size))
-    #print((hidden_layer_size+1)* num_labels)
-    theta1 = theta1.reshape((hidden_layer_size+1, num_labels))
+    theta1 = np.array(nn_params[0:(input_layer_size+1)*hidden_layer_size])
+    theta2 = np.array(nn_params[(input_layer_size+1)*hidden_layer_size:])
+    theta1 = theta1.reshape((hidden_layer_size, input_layer_size+1), order='F')
+    theta2 = theta2.reshape((num_labels, hidden_layer_size+1), order='F')
 
     m = X.shape[0]
     X = np.hstack((np.ones([m,1]),X))
+    a1 = X
+    z2 = np.dot(a1, theta1.T)
+    a2 = sigmoid(z2)
     
-    a1 = sigmoid(X.dot(theta0))
     
     #a1 = np.vstack((np.ones([1,a1.shape[1]]),a1)) 
-    a1 = np.append(np.ones([a1.shape[0], 1]),a1, 1) #Add bias
-    a2 = sigmoid(a1.dot(theta1))
+    a2 = np.append(np.ones([a1.shape[0], 1]),a2, 1) #Add bias
+
+    z3 = a2.dot(theta2.T)
+    h = sigmoid(z3)
     
     
     J = 0
     for row in range(m):
         for k in range(num_labels):
             valueY = y[row][k]
-            J = J + (-(valueY)*np.log(a2[row][k]) - (1-valueY)*np.log(1-a2[row][k]))
+            J = J + (-(valueY)*np.log(h[row][k]) - (1-valueY)*np.log(1-h[row][k]))
     J = (1/float(m))*J
-    J = J + (lam/float(2*m)) *(np.sum(np.square(theta0[:][2:]))+np.sum(np.square(theta1[:][2:])))
-    print J
+    J = J + (lam/float(2*m)) *(np.sum(np.square(theta1[:][2:]))+np.sum(np.square(theta2[:][2:])))
+    #print J
     return J
     
 def nn_gradient(nn_params, *args):
     input_layer_size, hidden_layer_size, num_labels, X, y, lam = args[0], args[1], args[2], args[3], args[4], args[5]
     
 
-    theta0 = np.array(nn_params[0:(input_layer_size+1)*hidden_layer_size])
-    theta1 = np.array(nn_params[(input_layer_size+1)*hidden_layer_size:])
-    theta0 = theta0.reshape((input_layer_size+1, hidden_layer_size))
-    theta1 = theta1.reshape((hidden_layer_size+1, num_labels))
+    theta1 = np.array(nn_params[0:(input_layer_size+1)*hidden_layer_size])
+    theta2 = np.array(nn_params[(input_layer_size+1)*hidden_layer_size:])
+    theta1 = theta1.reshape((hidden_layer_size, input_layer_size+1), order='F')
+    theta2 = theta2.reshape((num_labels, hidden_layer_size+1), order='F')
     m = X.shape[0]
     X = np.hstack((np.ones([m,1]),X))
     
     a1 = X
-    z2 = X.dot(theta0) 
+    z2 = X.dot(theta1.T) 
+    
     
     a2 = sigmoid(z2)
     #a1 = np.vstack((np.ones([1,a1.shape[1]]),a1)) 
     a2 = np.append(np.ones([a1.shape[0], 1]),a2, 1) #Add bias
 
-    z3 = a2.dot(theta1)
+    z3 = a2.dot(theta2.T)
     h = sigmoid(z3)
 
-    Theta0_grad = np.zeros(theta0.shape)
     Theta1_grad = np.zeros(theta1.shape)
+    Theta2_grad = np.zeros(theta2.shape)
     for t in range(m):
         
         a_1 = a1[t,:].reshape(1, input_layer_size+1)        
         z_2 = z2[t,:].reshape(1, hidden_layer_size)
         a_2 = a2[t,:].reshape(1, hidden_layer_size+1)
         
-        valueH = h[t, :].reshape(1, num_labels)
-        valueY = y[t].reshape(1, num_labels)
+        valueH = h[t, :].reshape(num_labels, 1)
+        valueY = y[t].reshape(num_labels, 1)
         delta3 = (valueH - valueY)
-        delta2 = np.dot(theta1,delta3.T)*(np.append(np.array([[1]]), sigmoidGradient(z_2), 1).T)
-        
+        delta2 = np.dot(theta2.T,delta3)*(np.append(np.array([[1]]), sigmoidGradient(z_2), 1)).T
         delta2 = delta2[1:]
              
-        
-        Theta0_grad += (np.dot(delta2,a_1)).T
-        Theta1_grad += np.dot(a_2.T, delta3)
+        Theta1_grad += (np.dot(delta2,a_1))
+        Theta2_grad += np.dot(delta3, a_2)
         
     #Regularize gradient
+    Theta2_grad = (1/float(m))*Theta2_grad + (float(lam)/m)*theta2
     Theta1_grad = (1/float(m))*Theta1_grad + (float(lam)/m)*theta1
-    Theta0_grad = (1/float(m))*Theta0_grad + (float(lam)/m)*theta0
 
-    grad = np.append(np.ndarray.flatten(Theta0_grad), np.ndarray.flatten(Theta1_grad))
+    grad = np.append(np.ndarray.flatten(Theta1_grad, order='F'), np.ndarray.flatten(Theta2_grad, order='F'))
     
     return grad
 
@@ -177,54 +185,70 @@ def load_every_xy_onevsall(m, val):
     #Y = np.reshape(Y, (m*10, 1))
     return X, Y
 
+def relativeDiff(x, xref):
+    #x = np.square(x)
+    #xref = np.square(xref)
+    return (x-xref)/xref
+
+def createMatrixFromY(y):
+    contentY = []
+    for val in y:
+        value = val[0]-1
+        valY = [0]*10
+        valY[value] = 1
+        contentY.append(np.array([valY]))
+        #print "{} ==> {}".format(value, valY)
+    Y = np.concatenate(contentY)
+    return Y
+        
 #Dimension of the hidden layer:
 hidden_dim = 25
-#X = np.array([ [0,0,1],[0,1,1],[1,0,1],[1,1,1] ])
-#X = load_x_for_val(3, 10)
-#y = np.array([[0, 0, 0, 1, 0, 0, 0, 0, 0, 0]]*10)
-oX, oy = load_every_xy_onevsall(20, 3)
-X = oX[:100]
-y = oy[:100]
-Xtest = oX[100:]
-ytest = oy[100:]
-#X, y = load_every_xy(20)
-#y = np.array([[0,1,1,0]]).T
+input_size = 400
+num_labels = 10
 
-epsilon_init = 0.12
+#oX, oy = load_every_xy_onevsall(10, 3)
+#X = oX
+#y = oy
+
+mat = sio.loadmat("ex4data1.mat")
+X = mat['X'][:10]
+y = createMatrixFromY(mat['y'])[:10]
+
+bob = sio.loadmat('ex4weights.mat')
+theta0, theta1 = bob['Theta1'], bob['Theta2']
+ThetaBob = np.append(np.ndarray.flatten(theta0, order='F'), np.ndarray.flatten(theta1, order='F'))
+
+
+epsilon_init = 0.0012
 theta0 = epsilon_init*2*np.random.random((X.shape[1]+1,hidden_dim))-epsilon_init
 theta1 = epsilon_init*2*np.random.random((hidden_dim+1,y.shape[1]))-epsilon_init
-
+Theta = np.append(np.ndarray.flatten(theta0, order='F'), np.ndarray.flatten(theta1, order='F'))
 #print theta1.shape
-Theta = np.concatenate([np.reshape(theta0, ((X.shape[1]+1)*hidden_dim, 1)), np.reshape(theta1, ((hidden_dim+1)*y.shape[1], 1))])
-print(nn_costfunction(Theta, X.shape[1], theta0.shape[1], y.shape[1], X, y, 0.5))
-#print(nn_gradient(Theta, X.shape[1], theta0.shape[1], y.shape[1], X, y, 0)-grad_check(nn_costfunction, Theta, args=(X.shape[1], theta0.shape[1], y.shape[1], X, y, 0)))
-#print(grad_check(nn_costfunction, Theta, args=(X.shape[1], theta0.shape[1], y.shape[1], X, y, 0.1)))
+#Theta = ThetaBob
+
+#Theta = np.concatenate([np.reshape(theta0, ((X.shape[1]+1)*hidden_dim, 1)), np.reshape(theta1, ((hidden_dim+1)*y.shape[1], 1))])
+
+print(nn_costfunction(Theta, input_size, hidden_dim, num_labels, X, y, 1))
+print(nn_gradient(Theta, input_size, hidden_dim, num_labels, X, y, 0.1)[:100])
+#print(relativeDiff(nn_gradient(Theta, input_size, hidden_dim, num_labels, X, y, 0), grad_check(nn_costfunction, Theta, args=( input_size, hidden_dim, num_labels, X, y, 0))))
+print(grad_check(nn_costfunction, Theta, args=(input_size, hidden_dim, num_labels, X, y, 0.1))[:100])
 
 
 
-print "Over"
-optTheta = scipy.optimize.fmin_cg(nn_costfunction, Theta, maxiter=400, args=( X.shape[1], theta0.shape[1], y.shape[1], X, y, 1), fprime=nn_gradient)
 
-'''
-for j in xrange(300):
-    a1 = sigmoid(np.dot(X, theta0))
-    a2 = sigmoid(np.dot(a1, theta1))
-    delta2 = (y-a2)*(a2*(1-a2))
-    delta1 = delta2.dot(theta1.T)*(a1*(1-a1))
-    theta1 += a1.T.dot(delta2)
-    theta0 += X.T.dot(delta1)
-'''
+optTheta = scipy.optimize.fmin_cg(nn_costfunction, Theta, maxiter=400, args=( input_size, hidden_dim, num_labels, X, y, 0), fprime=nn_gradient)
+#optTheta = scipy.optimize.fmin(nn_costfunction, Theta, maxiter=400, args=( input_size, hidden_dim, num_labels, X, y, 1))
 
-#Xtest, ytest = X, y
-Xtest, ytest = load_every_xy_onevsall(10, 3)
-result = predict(optTheta, X.shape[1], theta0.shape[1], y.shape[1], Xtest)
+Xtest, ytest = X, y
+#Xtest, ytest = load_every_xy_onevsall(10, 3)
+result = predict(optTheta, input_size, hidden_dim, num_labels, Xtest)
 good = 0
 total = Xtest.shape[0]
 for index, pred in enumerate(result):    
     #print("res = {}".format(pred))
-    print "Prediction n {} : ".format(index)    
-    print np.argmax(pred)
-    print np.argmax(ytest[index])
+    #print "Prediction n {} : ".format(index)    
+    #print np.argmax(pred)
+    #print np.argmax(ytest[index])
     if np.argmax(pred) == np.argmax(ytest[index]):
         good+=1
     
